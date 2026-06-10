7664a4df44e58dfc789e633463f97e8ef2e3e1c8
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2026-06-10 18:57:25 -0300

    Move to Vocs

---
+++ sdk/docs/vocs.config.ts
@@ -0,0 +1,114 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "PropAMM SDK",
+  description: "SDKs for interacting with the PropAMM contracts",
+  theme: {
+    accentColor: "#8051e7",
+  },
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/lambdaclass/propamm-router-contracts",
+    },
+  ],
+  sidebar: [
+    { text: "Introduction", link: "/" },
+    {
+      text: "TypeScript SDK",
+      items: [
+        { text: "Getting started", link: "/typescript/getting-started" },
+        {
+          text: "Quotes",
+          items: [
+            { text: "quote", link: "/typescript/router/quote" },
+            { text: "quoteVenue", link: "/typescript/router/quoteVenue" },
+            { text: "quoteSelectedVenues", link: "/typescript/router/quoteSelectedVenues" },
+            { text: "quoteUniswapV3", link: "/typescript/router/quoteUniswapV3" },
+          ],
+        },
+        {
+          text: "Swaps",
+          items: [
+            { text: "swap", link: "/typescript/router/swap" },
+            { text: "swapWithFee", link: "/typescript/router/swapWithFee" },
+            { text: "swapViaVenue", link: "/typescript/router/swapViaVenue" },
+            { text: "swapViaVenueWithFee", link: "/typescript/router/swapViaVenueWithFee" },
+            { text: "swapViaSelectedVenues", link: "/typescript/router/swapViaSelectedVenues" },
+            {
+              text: "swapViaSelectedVenuesWithFee",
+              link: "/typescript/router/swapViaSelectedVenuesWithFee",
+            },
+            { text: "waitForSwap", link: "/typescript/router/waitForSwap" },
+            { text: "frontendFee", link: "/typescript/router/frontendFee" },
+          ],
+        },
+        {
+          text: "Router",
+          items: [
+            { text: "approve", link: "/typescript/router/approve" },
+            { text: "allowance", link: "/typescript/router/allowance" },
+            { text: "Views", link: "/typescript/router/views" },
+          ],
+        },
+        {
+          text: "State overrides",
+          items: [
+            { text: "OverridesWsSource", link: "/typescript/overrides/ws-source" },
+            { text: "OverridesRpcSource", link: "/typescript/overrides/rpc-source" },
+            { text: "Utilities", link: "/typescript/overrides/utilities" },
+          ],
+        },
+        { text: "Helpers", link: "/typescript/helpers" },
+      ],
+    },
+    {
+      text: "Rust SDK",
+      items: [
+        { text: "Getting started", link: "/rust/getting-started" },
+        {
+          text: "Quotes",
+          items: [
+            { text: "quote", link: "/rust/router/quote" },
+            { text: "quote_venue", link: "/rust/router/quote_venue" },
+            { text: "quote_selected_venues", link: "/rust/router/quote_selected_venues" },
+            { text: "quote_uniswap_v3", link: "/rust/router/quote_uniswap_v3" },
+          ],
+        },
+        {
+          text: "Swaps",
+          items: [
+            { text: "swap", link: "/rust/router/swap" },
+            { text: "swap_with_fee", link: "/rust/router/swap_with_fee" },
+            { text: "swap_via_venue", link: "/rust/router/swap_via_venue" },
+            { text: "swap_via_venue_with_fee", link: "/rust/router/swap_via_venue_with_fee" },
+            { text: "swap_via_selected_venues", link: "/rust/router/swap_via_selected_venues" },
+            {
+              text: "swap_via_selected_venues_with_fee",
+              link: "/rust/router/swap_via_selected_venues_with_fee",
+            },
+            { text: "wait_for_swap", link: "/rust/router/wait_for_swap" },
+            { text: "frontend_fee", link: "/rust/router/frontend_fee" },
+          ],
+        },
+        {
+          text: "Router",
+          items: [
+            { text: "approve", link: "/rust/router/approve" },
+            { text: "allowance", link: "/rust/router/allowance" },
+            { text: "Views", link: "/rust/router/views" },
+          ],
+        },
+        {
+          text: "State overrides",
+          items: [
+            { text: "OverridesWsSource", link: "/rust/overrides/ws-source" },
+            { text: "OverridesRpcSource", link: "/rust/overrides/rpc-source" },
+            { text: "Utilities", link: "/rust/overrides/utilities" },
+          ],
+        },
+        { text: "Helpers", link: "/rust/helpers" },
+      ],
+    },
+  ],
+});
