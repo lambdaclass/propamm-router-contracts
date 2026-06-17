0bcfe8fba0d661f0542da1554942ea1f55acbe0f
ricomateo <mrico@fi.uba.ar>
2026-06-17 13:09:49 -0300

    update bebop address

--- sdk/python/src/propamm_sdk/common/pamms.py
+++ sdk/python/src/propamm_sdk/common/pamms.py
@@ -6,7 +6,7 @@ from eth_typing import ChecksumAddress
 from eth_utils import to_checksum_address
 
 FERMI: ChecksumAddress = to_checksum_address("0x5979458912F80B96d30D4220af8E2e4925A33320")
-BEBOP: ChecksumAddress = to_checksum_address("0x160141A205F5dDcf096BA3F48B7eD21EB52c62EA")
+BEBOP: ChecksumAddress = to_checksum_address("0xdB13ad0fcD134E9c48f2fDaEa8f6751a0F5349ca")
 KIPSELI: ChecksumAddress = to_checksum_address("0x71e790dd841c8A9061487cb3E78C288E75cE0B3d")
 
 #: Curated propAMM name -> venue address mapping, for the ``venues`` option of
